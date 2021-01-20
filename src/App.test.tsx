eb92958b80c758ea68f8b0a34856ec00b0b61763
Mimosa Poon <mimosapoon0703@gmail.com>
2021-01-20 16:52:06 +0000

    Project setup - add typescript config - add prettier - add scss

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders header text', () => {
+test("renders header text", () => {
   render(<App />);
   const linkElement = screen.getByText(/hello this is header/i);
   expect(linkElement).toBeInTheDocument();
--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import "./styles/styles.scss";
+
 function App() {
   return (
     <div className="app">
